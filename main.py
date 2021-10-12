d19a6e60f1cd69afffefcc98b2dac4a282dfeda7
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2021-10-12 19:08:18 +0100

    Update.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import analysis_tools.correlation_matrix as cm
 
 def main():
 
-    assets = funds.sp500_stocks()[110:150]
+    assets = funds.sp_500_stocks()[110:150]
 
     correlation_matrix = cm.correlation_matrix_generator(assets, period='1y', interval='1d', visual=True, img_name='Image dump/TEST')
     print(correlation_matrix)
